683795691e5f91aea8c23209a9b1a342fa4627a2
Waihon Yew <yewwaihon@gmail.com>
2019-03-04 18:16:00 +0800

    Stateless functional components

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import axios from 'axios';
+import Idea from './Idea';
 
 class IdeasContainer extends Component {
   constructor(props) {
@@ -22,12 +23,7 @@ class IdeasContainer extends Component {
     return (
       <div>
         {this.state.ideas.map((idea) => {
-          return(
-            <div className="tile" key={idea.id} >
-              <h4>{idea.title}</h4>
-              <p>{idea.body}</p>
-            </div>
-          )
+          return(<Idea idea={idea} key={idea.id} />)
         })}
       </div>
     );
