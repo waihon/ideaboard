78cd7316ce08234ad75ada8d92dd883227631bcb
Waihon Yew <yewwaihon@gmail.com>
2019-03-04 18:22:49 +0800

    Add new idea record

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Idea from './Idea';
+import update from 'immutability-helper';
 
 class IdeasContainer extends Component {
   constructor(props) {
@@ -19,9 +20,34 @@ class IdeasContainer extends Component {
       .catch(error => console.log(error));
   }
 
+  addNewIdea = () => {
+    axios.post(
+      'http://localhost:3001/api/v1/ideas',
+      { idea:
+        {
+          title: '',
+          body: ''
+        }
+      }
+    )
+      .then(response => {
+        console.log(response);
+        const ideas = update(this.state.ideas, {
+          $splice: [[0, 0, response.data]]
+        });
+        this.setState({ ideas: ideas });
+      })
+      .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div>
+        <div>
+          <button className="newIdeaButton" onClick={this.addNewIdea} >
+            New Idea
+          </button>
+        </div>
         {this.state.ideas.map((idea) => {
           return(<Idea idea={idea} key={idea.id} />)
         })}
