37794ac181cd880bda21d1d781c00c5e97c8e986
Waihon Yew <yewwaihon@gmail.com>
2019-03-09 11:05:02 +0800

    Delete an existing idea

--- src/components/Idea.js
+++ src/components/Idea.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 
 class Idea extends Component {
   handleClick = () => {
-    this.props.onClick(this.props.idea.id)
+    this.props.onClick(this.props.idea.id);
+  }
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.idea.id);
   }
 
   render() {
     return(
       <div className="tile">
+        <span className="deleteButton" onClick={this.handleDelete}>
+          x
+        </span>
         <h4 onClick={this.handleClick}>
           {this.props.idea.title}
         </h4>
--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -71,6 +71,18 @@ class IdeasContainer extends Component {
       () => { this.title.focus() });
   }
 
+  deleteIdea = (id) => {
+    axios.delete(`http://localhost:3001/api/v1/ideas/${id}`)
+      .then(response => {
+        const ideaIndex = this.state.ideas.findIndex(x => x.id === id);
+        const ideas = update(this.state.ideas, {
+          $splice: [[ideaIndex, 1]]
+        });
+        this.setState({ ideas: ideas });
+      })
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div>
@@ -89,7 +101,9 @@ class IdeasContainer extends Component {
               resetNotification={this.resetNotification}
               titleRef={input => this.title = input} />);
           } else {
-            return(<Idea idea={idea} key={idea.id} onClick={this.enableEditing} />);
+            return(<Idea idea={idea} key={idea.id}
+              onClick={this.enableEditing}
+              onDelete={this.deleteIdea} />);
           }
         })}
       </div>
